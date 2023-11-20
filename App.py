0e0b5c615db4d52aa1c8019803170d70f76b5503
Ignacio <ignacio.valencia.sauma@gmail.com>
2023-11-20 09:05:48 -0400

    plot image post predict

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import uvicorn
 import time
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
 from datetime import datetime
 import csv
 from fastapi import FastAPI, File, UploadFile, HTTPException
@@ -8,6 +8,7 @@ from fastapi.responses import HTMLResponse
 import cv2
 import numpy as np
 import io
+from matplotlib import pyplot as plt
 
 app = FastAPI()
 
@@ -31,19 +32,22 @@ def detect_objects(image):
     class_ids = []
     confidences = []
     boxes = []
+    human_count = 0  # Contador de humanos detectados
+
     for out in outs:
         for detection in out:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5:
-                # Object detected
+            if confidence > 0.5 and classes[class_id] == 'person':
+                # Object detected is a person
+                human_count += 1
+
+                # Rectangle coordinates
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
-
-                # Rectangle coordinates
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
 
@@ -51,7 +55,7 @@ def detect_objects(image):
                 confidences.append(float(confidence))
                 boxes.append([x, y, w, h])
 
-    return class_ids, confidences, boxes
+    return class_ids, confidences, boxes, human_count
 
 
 @app.post("/predict/")
@@ -59,24 +63,40 @@ async def detect_objects_endpoint(file: UploadFile = File(...)):
     contents = await file.read()
     image = cv2.imdecode(np.frombuffer(contents, np.uint8), -1)
 
-    start_time = time.time()  # Registrar el tiempo de inicio de la predicción
+    start_time = time.time()
 
-    class_ids, confidences, boxes = detect_objects(image)
+    class_ids, confidences, boxes, human_count = detect_objects(image)
 
-    end_time = time.time()  # Registrar el tiempo de finalización de la predicción
-    execution_time = end_time - start_time  # Calcular el tiempo de ejecución
+    end_time = time.time()
+    execution_time = end_time - start_time
 
-    # Almacenar información de la predicción para el reporte
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Guardar la imagen con los humanos encuadrados
+    for box in boxes:
+        x, y, w, h = box
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    save_path = f"./humans_detected/detected_{timestamp}.jpg"
+    cv2.imwrite(save_path, image)
+
+    # Actualizar la información del informe
     report_info = [file.filename, f"{image.shape[1]}x{image.shape[0]}",
-                   class_ids, timestamp, execution_time, "YOLOv3"]
+                   human_count, timestamp, execution_time, "YOLOv3", save_path]
     prediction_reports.append(report_info)
 
-    # Resto del código permanece igual
-    if 0 in class_ids:
-        return {"result": "There is an entity at home, is that you?", "execution_time": execution_time}
-    else:
-        return {"result": "All clear, nothing to worry by now.", "execution_time": execution_time}
+    # Mostrar la imagen con los encuadros en la respuesta
+    _, img_encoded = cv2.imencode('.png', image)
+    img_bytes = io.BytesIO(img_encoded.tobytes())
+
+    # Devolver la imagen junto con la respuesta JSON usando StreamingResponse
+    return StreamingResponse(img_bytes, media_type="image/png")
+
+
+@app.get("/download/{file_path}")
+async def download_detected_image(file_path: str):
+    file_path = f"./humans_detected/{file_path}"
+    return FileResponse(file_path)
 
 
 @app.get("/status")
@@ -117,7 +137,7 @@ async def get_reports():
         for report in prediction_reports:
             writer.writerow(report)
 
-    return FileResponse(file_name, filename="prediction_report.csv")
+    return FileResponse(file_name, filename="/reports/prediction_report.csv")
 
 
 def run_app():
